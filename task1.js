a294cc0b9054b316fb8e0e523ad92627a4b77ee2
vetrivel2003 <vetrivel452003@gmail.com>
2026-05-17 22:33:51 +0530

    JS Task1

---
+++ task1.js
@@ -0,0 +1,260 @@
+  //1. Create a variable using var and store your name.
+                var name = "Vetrivel"
+                
+        // 2. Print your name using console.log().
+                console.log(name);
+
+
+        // 3. Create a variable using let and store your age.
+                let age = 23
+
+
+        // 4. Change the value of age and print it.
+                age = 24
+                console.log(age);
+
+
+        // 5. Create a constant variable for country name.
+                const country = "India"
+                
+        // 6. Print the country name.
+                console.log(country)
+
+
+        // 7. Try redeclaring var variable and observe output.
+                var num1 = 77
+                var num1 = 11
+                console.log(num1);    //11
+
+        //8. Try redeclaring let variable and observe error
+                // let num2 = 26
+                // let num2 = 34
+                // console.log(a1);    //Uncaught SyntaxError: Identifier 'num2' has already been declared
+
+        // 9. Try reassigning const variable and observe error.
+                // const num3 = 6
+                // num3 = 10
+                // console.log(b1)       //Uncaught TypeError: Assignment to constant variable.
+
+
+        // 10. Write difference between var and let.
+                // var can be redeclared
+                // let cannot be redeclared.
+
+
+        // 11. Write difference between let and const.
+                // let can be reassigned.
+                // const cannot be redeclare and reassign.
+
+
+
+        // 12. Predict output
+
+                var a = 10
+                a = 20
+                var a = 30
+
+                console.log(a)       //30
+
+        // 13. Predict output
+
+                let b = 50
+                b = 60
+
+                console.log(b)       // 60
+
+
+
+        // 14. Predict output
+
+                const c = 100
+
+                console.log(c)  // 100
+
+
+        // 15. Use console.log() to print "Hello World".
+                console.log("Hello World")
+
+
+        // 16. Use alert() to show "Welcome User".
+                alert("Welcome User");
+
+
+        // 17. Use confirm() to ask user:
+        // "Do you like JavaScript?"
+
+                let userConfirm = confirm( "Do you like JavaScript?")
+                console.log(userConfirm);
+                
+
+        // 18. Use prompt() to ask user name.
+                let userName=  prompt ("Enter your name: ");
+               
+
+        // 19. Print prompt value in console.
+                console.log(userName);
+                
+        // 20. Use document.writeln() to print "Batch 33".
+                document.writeln("Batch 33")
+
+
+        // 21. Create variable studentName and store your name.
+                var studentName = "Vetrivel"
+
+
+        // 22. Create variable studentAge and store your age.
+                var studentAge = 23
+
+
+        // 23. Print both variables together.
+                console.log( studentName,studentAge)
+
+
+        // 24. Create a variable for mobile number.
+                let mobileNumber = 9876543210;
+
+
+        // 25. Print mobile number using console.log().
+
+                console.log(mobileNumber);
+
+
+        // 26. Create variable using var for favorite color.
+                var color = "green"
+
+
+        // 27. Reassign another color value.
+                color = "orange"
+
+
+        // 28. Create variable using let for marks.
+                let mark = 60
+
+
+        // 29. Change marks value and print it.
+                mark = 95
+                console.log(mark);
+                
+
+
+        // 30. Create const variable for PI value.
+                const PI = 3.33
+
+
+        // 31. Print PI value.
+                console.log(PI);        // 3.33
+
+
+        // 32. Predict output
+
+                var x = 5
+                var x = 15
+
+                console.log(x)       // 15
+
+
+        // 33. Predict output
+
+                let y = 25
+                y = 35
+
+                console.log(y)       // 35
+
+
+        // 34. Find error
+
+                // const z = 40
+                // z = 45
+
+                // console.log(z)       // Uncaught TypeError: Assignment to constant variable.
+
+
+        // 35. Create alert box with your college name.
+
+                alert("Shanmuga Industries Arts and Science College");       
+
+
+        // 36. Ask user age using prompt().
+
+                let userAge = prompt("Enter your age");
+
+
+        // 37. Print user age in console.   
+                console.log(userAge)
+
+
+        // 38. Ask user confirmation:
+        // "Did you complete HTML?"
+                let userCompletion = confirm("Did you complete HTML?..")
+
+
+        // 39. Print confirm result in console.
+                console.log(userCompletion);
+
+
+        // 40. Print "JavaScript Class" using document.writeln().
+                document.writeln("JavaScript Class");
+
+
+        // 41. Create 3 variables:
+        // city, state, country.    
+                let city = "Tiruvannamalai";
+                let state = "Tamil Nadu";
+                let country1 = "India";
+
+
+
+        // 42. Print all variables together.
+                console.log(city,state,country1);
+
+
+        // 43. Create one example for var.
+                var ex = 15;
+                ex =20;
+                var ex =25;
+                console.log(ex);
+
+
+        // 44. Create one example for let.
+                let ex1 = "vetri"
+                ex1 = "vetrivel"
+                console.log(ex1);
+
+        // 45. Create one example for const.
+                const ex2 = "Hello.."
+                console.log(ex2);
+
+
+
+        // 46. Predict output
+
+                var num = 100
+                num = 200
+
+                console.log(num)     // 200
+
+
+        // 47. Predict output
+
+                let price = 500
+                price = 700
+
+                console.log(price)   // 700
+
+
+        // 48. Predict output
+
+                const password = 12345
+
+                console.log(password)        // 12345
+
+        // 49. Write rules of var.
+                // var can be redeclared
+                // var can be reassigned
+                // var has global scope & function scope
+
+        // 50. Write rules of let and const.
+                // let cannot be redeclared
+                // let can be reassigned
+                // const cannot be redeclared
+                // const cannot be reassigned
